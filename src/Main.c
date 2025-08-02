7bdb3dea0f65a34d8e915e7451e9d66e0337c889
codeleaded <codeleaded@gmail.com>
2025-08-02 21:52:50 +0200

    added scroll and word alignment

--- src/Main.c
+++ src/Main.c
@@ -11,10 +11,12 @@
 
 TheaterSystem theater;
 
+Vector trace;
 TransformedView tv;
 Rect rect;
 Vec2 velocity;
 Vec2 acceleration;
+
 
 void Setup(AlxWindow* w){
 	theater = TheaterSystem_New(
@@ -24,10 +26,19 @@ void Setup(AlxWindow* w){
 		64
 	);
 
-	TheaterSystem_Set(&theater,0,(int[]){   },	TheaterOption_New(0,"David","NONE"));
-	TheaterSystem_Add(&theater,0,(int[]){   },	TheaterOption_New(1,"David","Hello World!"));
-	TheaterSystem_Add(&theater,1,(int[]){ 0 },	TheaterOption_New(2,"David","Its sooooo nice to meet you my friend!"));
+	TheaterSystem_Set(&theater,0,(int[]){   },		TheaterOption_New(0,"David","NONE"));
+	TheaterSystem_Add(&theater,0,(int[]){   },		TheaterOption_New(1,"David","Hello World!"));
+	TheaterSystem_Add(&theater,1,(int[]){ 0 },		TheaterOption_New(2,"David","Its sooooo nice to meet you my friend!"));
+	TheaterSystem_Add(&theater,2,(int[]){ 0,0 },	TheaterOption_New(3,"David","Its been a long way!"));
+	TheaterSystem_Add(&theater,3,(int[]){ 0,0,0 },	TheaterOption_New(
+		2,"David",
+		"Bro ive found something! Its the Programming Language C.\
+		Its simply the best! Wow ive never seen anything like that!\
+		Im so glad that you have come to listen to my words!"
+	));
 
+
+	trace = Vector_New(sizeof(int));
 
 	tv = TransformedView_New((Vec2){ GetWidth(),GetHeight() });
 	TransformedView_Offset(&tv,(Vec2){ 0.0f,0.0f });
@@ -41,6 +52,23 @@ void Update(AlxWindow* w){
 	TransformedView_HandlePanZoom(&tv,window.Strokes,(Vec2){ GetMouse().x,GetMouse().y });
 	
 	
+	if(Stroke(ALX_KEY_LEFT).PRESSED){
+		if(trace.size>0) Vector_PopTop(&trace);
+	}
+	if(Stroke(ALX_KEY_RIGHT).PRESSED){
+		Branch* b = Tree_Get_Branch(&theater.theater,trace.size,trace.Memory);
+		if(b->Childs.size>0) Vector_Push(&trace,(int[]){ 0 });
+	}
+
+	if(Stroke(ALX_KEY_UP).PRESSED){
+		TheaterSystem_Up(&theater,trace.size,trace.Memory);
+	}
+	if(Stroke(ALX_KEY_DOWN).PRESSED){
+		TheaterSystem_Down(&theater,trace.size,trace.Memory);
+	}
+	
+
+
 
 
 	velocity = Vec2_Add(velocity,Vec2_Mulf(acceleration,w->ElapsedTime));
@@ -76,10 +104,11 @@ void Update(AlxWindow* w){
 	Vec2 d = TransformedView_WorldScreenLength(&tv,rect.d);
 	RenderRect(p.x,p.y,d.x,d.y,RED);
 
-	
+	TheaterSystem_Render(WINDOW_STD_ARGS,&theater,0.0f,100.0f,trace.size,trace.Memory);
 }
 void Delete(AlxWindow* w){
-    
+	Vector_Free(&trace);
+    TheaterSystem_Free(&theater);
 }
 
 int main(){
